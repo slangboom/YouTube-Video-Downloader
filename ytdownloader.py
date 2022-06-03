fb00e38f76572c55196a63010dcb4ea100d5e74f
slangboom <abhisheknyamati@gmail.com>
2022-06-03 19:54:33 +0530

    only highest reso

---
+++ ytdownloader.py
@@ -0,0 +1,30 @@
+from pytube import YouTube
+
+# link = "https://www.youtube.com/watch?v=NpM7K-ygjxY" # my dad has a gtr
+# link = "https://www.youtube.com/watch?v=4v0-dBoaKB8" # zoro
+link = "https://www.youtube.com/watch?v=TWAPqCbPfiU"
+my_video = YouTube(link)
+
+print("*********************video title******************************")
+print()
+print(my_video.title)
+print()
+
+# my_video = my_video.streams.get_highest_resolution()########3
+
+video_quality = my_video.streams.all()
+# video_quality = my_video.streams.filter()       # filter
+video_quality_list = list(enumerate(video_quality))
+
+# for i in video_quality_list:
+#     print(i)
+  
+# print()
+
+# strm = int(input("enter: "))
+
+# print("*********************download initiated******************************")
+# my_video[strm].download()
+
+# # my_video.download()##########
+# print("*********************download successful******************************")
