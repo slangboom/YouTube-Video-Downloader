f2bf87355c925c5258fe2fea074e2d67a687ed7c
slangboom <abhisheknyamati@gmail.com>
2022-06-03 20:24:29 +0530

    only highest reso

--- ytdownloader.py
+++ ytdownloader.py
@@ -10,11 +10,12 @@ print()
 print(my_video.title)
 print()
 
-# my_video = my_video.streams.get_highest_resolution()########3
+print("********************* pre reso ******************************")
+my_video = my_video.streams.get_highest_resolution()
 
-video_quality = my_video.streams.all()
+# video_quality = my_video.streams.all()
 # video_quality = my_video.streams.filter()       # filter
-video_quality_list = list(enumerate(video_quality))
+# video_quality_list = list(enumerate(video_quality))
 
 # for i in video_quality_list:
 #     print(i)
@@ -22,9 +23,8 @@ video_quality_list = list(enumerate(video_quality))
 # print()
 
 # strm = int(input("enter: "))
-
-# print("*********************download initiated******************************")
+print("*********************download initiated******************************")
 # my_video[strm].download()
 
-# # my_video.download()##########
-# print("*********************download successful******************************")
+my_video.download()
+print("*********************download successful******************************")
