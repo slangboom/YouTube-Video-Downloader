db7482aba9f3a5e10cfc1b6add91cc0759d42d34
slangboom <abhisheknyamati@gmail.com>
2022-06-03 20:30:46 +0530

    only highest reso

--- ytdownloader.py
+++ ytdownloader.py
@@ -16,13 +16,11 @@ my_video = my_video.streams.get_highest_resolution()
 # video_quality = my_video.streams.all()
 # video_quality = my_video.streams.filter()       # filter
 # video_quality_list = list(enumerate(video_quality))
-
 # for i in video_quality_list:
 #     print(i)
-  
 # print()
-
 # strm = int(input("enter: "))
+
 print("*********************download initiated******************************")
 # my_video[strm].download()
 
